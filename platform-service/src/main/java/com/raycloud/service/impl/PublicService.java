3ded50f5de19a5eacf1e625147237cb7d0e140f3
linjunjie <linjunjie@raycloud.com>
2016-07-09 14:54:50 +0800

    整理工程

---
+++ platform-service/src/main/java/com/raycloud/service/impl/PublicService.java
@@ -0,0 +1,29 @@
+package com.raycloud.service.impl;
+
+import com.raycloud.exception.ServiceException;
+import com.raycloud.pojo.Category;
+import com.raycloud.pojo.User;
+import com.raycloud.pojo.Video;
+import com.raycloud.request.*;
+import com.raycloud.response.ViewCategoryList;
+import com.raycloud.response.ViewPreVideoList;
+import com.raycloud.response.ViewVideoList;
+import com.raycloud.util.ContentTypeUtil;
+import com.raycloud.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by linjunjie(490456661@qq.com) on 2016/6/3.
+ */
+@Service
+public class PublicService {
+
+
+
+}
